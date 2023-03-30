096de043949635a375ca09fa800d447d9fb8ad93
karam-zomlut <zomlutk@gmail.com>
2023-03-30 17:37:27 +0200

    Applying Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,20 @@ import React, { FormEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { FieldValues } from 'react-hook-form/dist/types';
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
-
+  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className='mb-3'>
@@ -14,22 +23,34 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register('name')}
+          {...register('name', { required: true, minLength: 3 })}
           id='name'
           type='text'
-          className='form-control'
+          className={`form-control ${errors.name?.type ? 'is-invalid' : ''}`}
         />
+        {errors.name?.type === 'required' && (
+          <p className='invalid-feedback'>The name field is required!</p>
+        )}
+        {errors.name?.type === 'maxLength' && (
+          <p className='invalid-feedback'>The name must be at least 3 characters</p>
+        )}
       </div>
       <div className='mb-3'>
         <label htmlFor='age' className='form-label'>
           Age
         </label>
         <input
-          {...register('age')}
+          {...register('age', { required: true, min: 1 })}
           id='age'
           type='number'
-          className='form-control'
+          className={`form-control ${errors.age?.type ? 'is-invalid' : ''}`}
         />
+        {errors.age?.type === 'required' && (
+          <p className='invalid-feedback'>The age field is required!</p>
+        )}
+        {errors.age?.type === 'min' && (
+          <p className='invalid-feedback'>The age value unacceptable!</p>
+        )}
       </div>
       <button className='btn btn-primary' type='submit'>
         Submit
