821aa17b19b78635d728e0807654735818b1ef60
karam-zomlut <zomlutk@gmail.com>
2023-03-30 16:04:41 +0200

    Controlled Components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,20 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { FormEvent, useState } from 'react';
 
+let renders = 0;
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: '', age: 0 };
+  console.log(renders);
+  renders++;
+  const [person, setPerson] = useState({
+    name: '',
+    age: '',
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
+
+  
 
   return (
     <form onSubmit={handleSubmit}>
@@ -18,13 +22,25 @@ const Form = () => {
         <label htmlFor='name' className='form-label'>
           Name
         </label>
-        <input ref={nameRef} id='name' type='text' className='form-control' />
+        <input
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          id='name'
+          type='text'
+          className='form-control'
+          value={person.name}
+        />
       </div>
       <div className='mb-3'>
         <label htmlFor='age' className='form-label'>
           Age
         </label>
-        <input ref={ageRef} id='age' type='number' className='form-control' />
+        <input
+          onChange={(e) => setPerson({ ...person, age: e.target.value })}
+          id='age'
+          type='number'
+          className='form-control'
+          value={person.age}
+        />
       </div>
       <button className='btn btn-primary' type='submit'>
         Submit
