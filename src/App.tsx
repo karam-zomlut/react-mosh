3fa0d2251f77afbb2b977b814af974caac4a73e4
karam-zomlut <zomlutk@gmail.com>
2023-04-22 21:14:20 +0200

    Handling Errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
-import { ProductList } from './components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 function App() {
-  const [category, setCategory] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        setUsers(res.data);
+        setError('');
+      })
+      .catch((err) => setError(err.message));
+  }, []);
+
+  if (error) {
+    return (
+      <div className='alert alert-danger' role='alert'>
+        {error}
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <select
-        className='form-select'
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=''></option>
-        <option value='Clothing'>Clothing</option>
-        <option value='Household'>Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
+    <>
+      {/* {error && (
+        <div className='alert alert-danger' role='alert'>
+          {error}
+        </div>
+      )} */}
+      <ul>
+        {users?.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
