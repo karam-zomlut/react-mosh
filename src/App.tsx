48fd9d38e873a179130e332792cc6eed5b377a07
karam-zomlut <zomlutk@gmail.com>
2023-04-23 21:22:41 +0200

    Cancelling fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 import { useEffect, useState } from 'react';
 
 interface User {
@@ -12,30 +12,39 @@ function App() {
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
+
     axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
       .then((res) => {
         setUsers(res.data);
-        setError('');
+        // setError('');
       })
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
 
-  if (error) {
-    return (
-      <div className='alert alert-danger' role='alert'>
-        {error}
-      </div>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <div className='alert alert-danger' role='alert'>
+  //       {error}
+  //     </div>
+  //   );
+  // }
 
   return (
     <>
-      {/* {error && (
+      {error && (
         <div className='alert alert-danger' role='alert'>
           {error}
         </div>
-      )} */}
+      )}
       <ul>
         {users?.map((user) => (
           <li key={user.id}>{user.name}</li>
