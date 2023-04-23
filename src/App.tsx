2d19acaacb2969026b1b2361d24154e878b04bde
karam-zomlut <zomlutk@gmail.com>
2023-04-23 23:54:01 +0200

    Create Custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import { useEffect, useState } from 'react';
 import { CanceledError, User, userServices } from './services';
+import { useUsers } from './hooks';
 
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const { request, cancel } = userServices.getAll<User>();
-      request.then((res) => {
-        setUsers(res.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, isLoading, setUsers, setError, error } = useUsers();
 
   const addUser = () => {
     const originalUsers = [...users];
