b2d1c24a10faba10989a16353aa28d4ea3e5ce5d
karam-zomlut <zomlutk@gmail.com>
2023-04-23 22:42:07 +0200

    Updating Data

--- src/App.tsx
+++ src/App.tsx
@@ -37,24 +37,38 @@ function App() {
     const newUser = {
       id: Date.now(),
       name: 'New User',
-    }
+    };
 
     setUsers([newUser, ...users]);
 
     axios
       .post('https://jsonplaceholder.typicode.com/users', newUser)
-      .then(({data: savedUser}) => {
+      .then(({ data: savedUser }) => {
         setUsers([savedUser, ...users]);
       })
       .catch((err) => {
         setError(err.message);
         setUsers(originalUsers);
       });
-  }
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + ' updated' };
+
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch(`https://jsonplaceholder.typicode.com/users/${user.id}`, updateUser)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
   const deleteUser = (user: User) => {
-    const originalUsers = [...users]
-    setUsers(users.filter(u => u.id !== user.id));
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
 
     axios
       .delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
@@ -62,7 +76,7 @@ function App() {
         setError(err.message);
         setUsers(originalUsers);
       });
-  }
+  };
 
   // if (error) {
   //   return (
@@ -80,12 +94,30 @@ function App() {
         </div>
       )}
       {isLoading && <div className='spinner-border'></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+      <button className='btn btn-primary mb-3' onClick={addUser}>
+        Add
+      </button>
       <ul className='list-group'>
         {users?.map((user) => (
-          <li key={user.id} className='list-group-item d-flex justify-content-between'>
-            {user.name}{' '}{user.id}
-            <button className='btn btn-outline-danger' onClick={() => deleteUser(user)}>Delete</button>
+          <li
+            key={user.id}
+            className='list-group-item d-flex justify-content-between'
+          >
+            {user.name}
+            <div>
+              <button
+                className='btn btn-outline-secondary mx-1'
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className='btn btn-outline-danger'
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
